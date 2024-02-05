ea39f1b16e0e10f10515a44dfdcbd73bfab4329b
Isaac Díaz López <isaac.diaz.lopez@hotmail.com>
2024-02-05 23:00:25 +0100

    NuevoCliente, Formulario, Errores. action loader

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 function Layout() {
+    const location = useLocation()
+
+    console.log(location)
     return (
         <div className='md:flex md:min-h-screen'>
             <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
                 <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
 
                 <nav className='mt-10'>
-                    <a href='/'>Clientes</a>
-                    <a href='/clientes/nuevo'>Nuevo Cliente</a>
+                    <Link className='text-2xl block mt-2 text-blue-300 text-white' to='/'>Clientes</Link>
+                    <Link className='text-2xl block mt-2 text-blue-300 text-white' to='/clientes/nuevo'>Nuevo Cliente</Link>
                 </nav>
             </aside>
 
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,60 @@
+import { useNavigate, Form, useActionData } from "react-router-dom"
+import Formulario from "../components/Formulario"
+import Error from "../components/Error"
+
+export async function action({ request }) {
+    const formData = await request.formData()
+
+    const datos = Object.fromEntries(formData)
+
+    //Validación
+    const errores = []
+    if (Object.values(datos).includes('')) {
+        errores.push('Todos los campos son obligatorios')
+    }
+
+    //Retornar errores
+    if (Object.keys(errores).lenght) {
+        return errores
+    }
+}
+
 function NuevoCliente() {
+
+    const errores = useActionData()
+    const navigate = useNavigate()
+
+    console.log(errores)
     return (
-        <div>NuevoCliente</div>
+        <>
+            <h1 className='font-black text-4xl text-blue-900'>Nuevo cliente</h1>
+            <p className='mt-3'>Llena todos los campos para registrar un nuevo cliente</p>
+
+            <div className='flex justify-end'>
+                <button className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'
+                    onClick={() => navigate('/')}>
+                    Volver
+                </button>
+            </div>
+
+
+            <div className='bg-white shadow rounded-md md:3/4 mx-auto px-5 py-10 mt-20'>
+
+                {errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error>)}
+
+                <Form
+                    method='post'
+                >
+                    <Formulario />
+
+                    <input
+                        type='submit'
+                        className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+                        value='Registrar cliente'
+                    />
+                </Form>
+            </div>
+        </>
     )
 }
 
