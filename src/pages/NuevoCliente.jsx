e8b6bfe6b50fc2158e44a27cbf989a650f4b8f23
Isaac Díaz López <isaac.diaz.lopez@hotmail.com>
2024-02-06 19:14:31 +0100

    Json server, API Rest, Validación Formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -7,6 +7,14 @@ export async function action({ request }) {
 
     const datos = Object.fromEntries(formData)
 
+    const email = formData.get('email')
+
+    let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+    if(!regex.test(email)) {
+        errores.push('El Email no es valido')
+    }
+
     //Validación
     const errores = []
     if (Object.values(datos).includes('')) {
@@ -44,6 +52,7 @@ function NuevoCliente() {
 
                 <Form
                     method='post'
+                    noValidate
                 >
                     <Formulario />
 
